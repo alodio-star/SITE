bc6fcf8f558fbe9cd7840be26eacd4f7aff48009
alodio-star <alodiom2007@gmail.com>
2024-09-24 20:40:10 -0400

    Add files via upload

---
+++ inde.php
@@ -0,0 +1,20 @@
+<?php
+$email = $_POST['mail'];
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = '$email';
+$asunto = 'tu vuelo a sido comprado ';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
